9f40a50e665dcf58f7949915939be946c0d82d52
Léo Levacher <leo.levacher@sogeti.com>
2023-08-29 22:18:39 +0200

    fix: title container layout

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,18 +1,18 @@
 import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const HomePage = lazy(() => import('Pages/Home/Home'));
+const HomePage = lazy(() => import('pages/Home/Home'));
 
 const AppRoutes = () => {
-	return (
-		<BrowserRouter>
-			<Suspense fallback={<></>}>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-				</Routes>
-			</Suspense>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default AppRoutes;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,16 +3,18 @@ import codeTyping from 'assets/illustrations/Code typing-amico.svg';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
-	return (
-		<div className={styles.container}>
-			<h1 className={styles.title}>
-				Seamless solutions,
-				<br />
-				Serenely crafted
-			</h1>
-			<img src={codeTyping} alt="Code typing" />
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>
+          Seamless solutions,
+          <br />
+          Serenely crafted
+        </h1>
+        <img src={codeTyping} alt="Code typing" className={styles.workingImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
